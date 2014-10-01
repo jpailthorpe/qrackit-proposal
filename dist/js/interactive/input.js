492a9bb4eb954799ef5653951ad6ba804754de7a
Jay Vachon <jaylvachon@gmail.com>
2014-10-01 15:07:52 -0400

    message animation

--- dist/js/interactive/input.js
+++ dist/js/interactive/input.js
@@ -12,8 +12,15 @@
 	_bindEvents();
 
 	function _bindEvents() {
-		$BODY.on('click', '.col-12 .overlay', function(e) {
-			console.log('click!');
+		$BODY.on('click', '.animation-trigger', function(e) {
+			var container = $('.message');
+			container.show();
+			container.css('height', 0);
+			container.css('top', 350);
+			container.animate({
+				top: 15,
+				height: 350
+			}, 1000);
 		});
 	}
 
