a2ed4f262ffe2cb000713964b5b3a7069b657259
Jay Vachon <jaylvachon@gmail.com>
2014-10-03 15:44:08 -0400

    added cairn animation

--- dist/js/interactive/input.js
+++ dist/js/interactive/input.js
@@ -7,6 +7,33 @@
 		showingComment1: false,
 		showingComment2: false,
 
+		rockPositions: [
+
+			// Cairn 1
+			[0.1, 0.6],
+			[0.15, 0.6],
+			[0.2, 0.6],
+			[0.12, 0.55],
+			[0.17, 0.55],
+			[0.15, 0.5],
+
+			// Cairn 2
+			[0.75, 0.6],
+			[0.8, 0.6],
+			[0.85, 0.6],
+			[0.77, 0.55],
+			[0.82, 0.55],
+			[0.8, 0.5],
+
+			// Cairn 3
+			[0.45, 0.8],
+			[0.5, 0.8],
+			[0.55, 0.8],
+			[0.47, 0.75],
+			[0.52, 0.75],
+			[0.5, 0.7]
+		],
+
 		init: function() {
 			console.log('input ready');
 		}
@@ -16,7 +43,24 @@
 	_bindEvents();
 
 	function _bindEvents() {
-		$BODY.on('click', '#relative-col', function(e) {
+		$BODY.on('click', '#cairn-col', function(e) {
+			var col = $('#cairn-col');
+			for (var i = 0; i < input.rockPositions.length; i ++) {
+				_moveRock(i + 1, col.width(), col.height());
+			}
+		});
+
+		function _moveRock(index, divWidth, divHeight) {
+			var rock = $('#rock-' + index);
+			rock.css('top', divHeight * 0.5);
+			rock.css('left', divWidth * 0.5);
+			rock.animate({
+				left: divWidth * input.rockPositions[index - 1][0],
+				top: divHeight * input.rockPositions[index - 1][1]
+			}, 200 + Math.random() * 500);
+		}
+
+		$BODY.on('click', '#message-col', function(e) {
 			var messagebox = $('.message-box');
 			var comment1 = $('#comment-1');
 			var comment2 = $('#comment-2');
@@ -24,6 +68,7 @@
 				messagebox.show();
 				messagebox.css('height', 0);
 				messagebox.css('margin-top', 350);
+				messagebox.css('opacity', 1);
 				messagebox.animate({
 					marginTop: 15,
 					height: 350
@@ -41,9 +86,8 @@
 					});
 				} else {
 					messagebox.animate({
-						marginTop: 350,
-						height: 0
-					}, 1000, function() {
+						opacity: 0
+					}, 500, function() {
 						comment1.hide();
 						comment2.hide();
 						input.showingMessage = false;
