b788f5ad7107249f9a5601cefb1b627ffaaba2d6
Jay Vachon <jaylvachon@gmail.com>
2014-10-07 18:06:25 -0400

    better responsiveness

--- dist/js/interactive/main.js
+++ dist/js/interactive/main.js
@@ -1,5 +1,36 @@
+var scrollElement = null,
+	w = null,
+	resizeTimer = null,
+	height = 600;
+
 // Main loop
 (function(){
+
+	scrollElement = $('html, body');
+	w = $(window);
+
+	scrollElement.each(function(i) {
+		var initScrollTop = $(this).attr('scrollTop');
+		$(this).attr('scrollTop', 0);
+	});
+
+	w.on('resize', function () {
+		if (resizeTimer) {
+			clearTimeout(resizeTimer);
+		}
+
+		resizeSections();
+		resizeTimer = setTimeout(function() {
+			var s = w.scrollTop(),
+				newPos = Math.round(s / height) * height;
+			resizeSections();
+			scrollElement.stop().animate({
+				scrollTop: newPos
+			}, 250);
+		}, 500);
+	});
+
+	resizeSections();
 
 	window.$main = {
 
@@ -20,3 +51,12 @@
 	}
 
 })();
+
+function resizeSections () {
+	var h = w.height();
+	height = h < 640 ? 640 : h;
+	$('.row').css('height', height);
+	var offset = (height - 600) * 0.5;
+	$('.wrapper').css('padding-top', offset * 0.5);
+	$('.wrapper').css('padding-bottom', offset * 0.5);
+}
